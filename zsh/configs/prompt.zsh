334198850b742bd2997b8f6a3051e70a6be4cf0f
Kentama7 <kentama.7m@gmail.com>
2020-12-12 10:53:11 +0900

    Add typewritten and zsh-syntax-highlighting

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,5 +1,4 @@
 # see at https://github.com/reobin/typewritten
-fpath+=$HOME/.zsh/typewritten
-autoload -U promptinit; promptinit
-prompt typewritten
+TYPEWRITTEN_RELATIVE_PATH="adaptive"
+TYPEWRITTEN_COLOR_MAPPINGS="primary:#9580FF;secondary:#8AFF80;accent:#FFFF80;info_negative:#FF80BF;info_positive:#8AFF80;info_neutral_1:#FF9580;info_neutral_2:#FFFF80;info_special:#80FFEA"
 
